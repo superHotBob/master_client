2a31c4b42eb29fa7c2dfb26c5fc8339ba0b3cc45
superHotBob <19ham09@gmail.com>
2023-03-03 19:57:24 +0300

    map_2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,19 +2,22 @@ import '../styles/globals.css'
 import { Rubik } from 'next/font/google'
 import { store } from '../store'
 import { Provider } from 'react-redux'
-
+import Head from 'next/head'
 
 const rubik = Rubik({
-  weight: ['400','500'],
+  weight: ['400', '500'],
   subsets: ['latin'],
 })
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-    <main className={rubik.className}>
-      <Component {...pageProps} />
-    </main>
+      <main className={rubik.className}>
+        <Head >
+          <script src="https://api-maps.yandex.ru/3.0/?apikey=89caab37-749d-4e30-8fdf-e8045542f060&lang=ru_RU"></script>
+        </Head>
+        <Component {...pageProps} />
+      </main>
     </Provider>
   )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, Head, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <head>
-        <script src="https://api-maps.yandex.ru/3.0/?apikey=89caab37-749d-4e30-8fdf-e8045542f060&lang=ru_RU"></script>
-      </head>
       <body>
         <Main />
         <NextScript />
