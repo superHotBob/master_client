ac22fa6386a784781108a45b6d4919ce8b89735b
superHotBob <19ham09@gmail.com>
2023-03-24 16:30:14 +0300

    replace desptuct

--- src/pages/masterprofile/[pid].js
+++ src/pages/masterprofile/[pid].js
@@ -3,7 +3,8 @@ import { useRouter } from 'next/router'
 import Header from '@/components/header'
 import Head from 'next/head'
 import Link from 'next/link'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { setprofile } from '@/reduser'
 import { useEffect, useState } from 'react'
 import Message from '@/components/message'
 import Navi from '@/components/navi'
@@ -18,77 +19,89 @@ const nav_active = {
 export default function Client() {
     const router = useRouter()
     const { pid } = router.query
-    const profile = useSelector((state) => state.counter.profile)    
+    const [profile, setProfile] = useState()
     const [nav_view, setNavView] = useState('Лента')
     const [nav_active, setNavActive] = useState()
-    const [one, two, three] = profile.color
 
-    useEffect(() => profile.status === 'master' ? setNavActive({
-        backgroundColor: two,
-        color: "#fff",
-        padding: '0 20px',
-        fontWeight: 600
-    }) : () => router.push('/enter'), [two, router,profile.status])
+
+
+
+    useEffect(() => {
+        let pro = JSON.parse(localStorage.getItem("profile"))
+        setProfile(profile => ({ ...profile, ...pro }))       
+        if (pro.status === 'master') {
+            setNavActive({
+                backgroundColor: pro.color[1],
+                color: "#fff",
+                padding: '0 20px',
+                fontWeight: 600
+            })
+        } else {
+            () => router.push('/enter')
+        }
+    }, [])
     return (
         <main className={styles.main}>
             <Head>
                 <title>{pid}</title>
             </Head>
-            <Header text={pid} color={profile.color} />
-            <div
-                className={styles.profile}
-                style={{ backgroundImage: profile.image ? 'url(' + profile.image + ')' : null }}
-            >
-                {profile.name}
-                <Link href="/editprofile" style={{color: two}}>{profile.text || 'Написать о себе'}</Link>
-            </div>
-            <nav className={styles.navigation}>
-                {['Лента', 'Услуги', 'Сертификаты', 'Отзывы']
-                    .map(i => <span key={i} onClick={() => setNavView(i)} style={nav_view === i ? nav_active : null}>{i}</span>)}
-            </nav>
+            {profile ? <>
+                <Header text={pid} color={profile.color} />
+                <div
+                    className={styles.profile}
+                    style={{ backgroundImage: profile.image ? 'url(' + profile.image + ')' : null }}
+                >
+                    {profile.name}
+                    <Link href="/editprofile" style={{ color: profile.color[1] }}>{profile.text || 'Написать о себе'}</Link>
+                </div>
+                <nav className={styles.navigation}>
+                    {['Лента', 'Услуги', 'Сертификаты', 'Отзывы']
+                        .map(i => <span key={i} onClick={() => setNavView(i)} style={nav_view === i ? nav_active : null}>{i}</span>)}
+                </nav>
 
-            <section className={styles.lenta}>
+                <section className={styles.lenta}>
 
-                {nav_view === 'Отзывы' ? <>
-                    <Message color={profile.color} text={`Здесь будут отображаться отзывы на выполенные
+                    {nav_view === 'Отзывы' ? <>
+                        <Message color={profile.color} text={`Здесь будут отображаться отзывы на выполенные
                         вами заказы и проведенные мероприятия.`} />
 
-                </> : null}
-                {nav_view === 'Услуги' ? <>
-                    <Message color={profile.color} text={`Здесь будут отображаться ваши слуги  прайс лиcт 
+                    </> : null}
+                    {nav_view === 'Услуги' ? <>
+                        <Message color={profile.color} text={`Здесь будут отображаться ваши слуги  прайс лиcт 
                     по категориям. Вы сможете редактировать его в
                     любое время, дополняя и редактируя его.`} />
-                    <Link href="/addservice" className={styles.uslugi} style={{backgroundColor: two }}>
-                        <span style={{ color: '#fff'}}> Добавить услугу</span>
-                    </Link>
-                </> : null}
+                        <Link href="/addservice" className={styles.uslugi} style={{ backgroundColor: profile.color[1] }}>
+                            <span style={{ color: '#fff' }}> Добавить услугу</span>
+                        </Link>
+                    </> : null}
 
-                {nav_view === 'Лента' ? <>
-                    <Message color={profile.color} text={`Здесь будут фотографии ваших работ, которые 
+                    {nav_view === 'Лента' ? <>
+                        <Message color={profile.color} text={`Здесь будут фотографии ваших работ, которые 
                     будут видны клиентам и другим мастерам. Они
                     смогут их сохранять в закладки, что бы не
                     потерять вас из виду.
             `} />
 
-                    <Link
-                        href="/masternear"
-                        className={styles.uslugi}
-                        style={{ color: '#fff', backgroundColor: two }}
-                    >
-                        <span style={{ color: '#fff'}}>Добавить публикацию</span>
-                    </Link>
-                </> : null}
-                {nav_view === 'Сертификаты' ? <>
-                    <Message color={profile.color} text={`Расскажите о своем профессиональном опыте,
+                        <Link
+                            href="/masternear"
+                            className={styles.uslugi}
+                            style={{ color: '#fff', backgroundColor: profile.color[1] }}
+                        >
+                            <span style={{ color: '#fff' }}>Добавить публикацию</span>
+                        </Link>
+                    </> : null}
+                    {nav_view === 'Сертификаты' ? <>
+                        <Message color={profile.color} text={`Расскажите о своем профессиональном опыте,
                         продемонстрируйте всем свое мастеркство и
                         продтвердите это сертификатами.                    
                     `} />
-                    <Link href="/masternear" className={styles.uslugi} style={{ color: '#fff', backgroundColor: two }}>
-                    <span style={{ color: '#fff'}}>Добавить сетрификат</span>
-                    </Link>
-                </> : null}
-            </section>
-            <Navi color={two} />
+                        <Link href="/masternear" className={styles.uslugi} style={{ color: '#fff', backgroundColor: profile.color[1] }}>
+                            <span style={{ color: '#fff' }}>Добавить сетрификат</span>
+                        </Link>
+                    </> : null}
+                </section>
+                <Navi color={profile.color[1]} />
+            </> : null}
         </main >
     )
 }
