db8caf39013667bf3c39b5fd1eba09e37b56b4f9
superHotBob <19ham09@gmail.com>
2023-07-25 13:19:12 +0300

    new_function

--- src/pages/confirmation/index.js
+++ src/pages/confirmation/index.js
@@ -67,17 +67,21 @@ export default function Confirmation() {
 
     }
        
-    function Order_Date(a = "1,'Июнь',10:00") {        
-        const dt = new Date()
-        let d = a.split(',')      
-        const tm = d[2].split(':')
-        console.log(tm)
-        const date_ord = new Date(dt.getFullYear(),months.indexOf(d[1]), d[0], tm[0],tm[1]);
-        const new_date = Date.parse(date_ord)
-        const date= new Date(new_date)
-        const formattedDate = date_ord.toLocaleDateString('ru-RU', { year: 'numeric', month: 'long', day: 'numeric', hour:   '2-digit',
-        minute: '2-digit', });
-        return formattedDate
+    function Order_Date(a = "1,'Июнь',10:00") { 
+        if(a) {            
+            const dt = new Date()
+            let d = a.split(',')      
+            const tm = d[2].split(':')
+            console.log(tm)
+            const date_ord = new Date(dt.getFullYear(),months.indexOf(d[1]), d[0], tm[0],tm[1]);
+            const new_date = Date.parse(date_ord)
+            const date= new Date(new_date)
+            const formattedDate = date_ord.toLocaleDateString('ru-RU', { year: 'numeric', month: 'long', day: 'numeric', hour:   '2-digit',
+            minute: '2-digit', });
+            return formattedDate
+        } else {
+            return 
+        }  
     }
     
     return (
