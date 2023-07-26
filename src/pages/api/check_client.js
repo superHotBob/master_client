16f043e7c0f6382e0ce7660e29f1483e7ecae0f7
superHotBob <19ham09@gmail.com>
2023-07-26 15:15:55 +0300

    new_ip

--- src/pages/api/check_client.js
+++ src/pages/api/check_client.js
@@ -1,5 +1,5 @@
 import postgres from "postgres"
-
+const IP = require('ip')
 export default async function handler(req, res) {
     const sql = postgres(`postgres://bobozeranski:${process.env.DATABASE_API}@ep-yellow-mountain-679652.eu-central-1.aws.neon.tech/neondb?sslmode=require&options=project%3Dep-yellow-mountain-679652`)
 
@@ -10,11 +10,12 @@ export default async function handler(req, res) {
         where phone = ${req.query.phone}
     `
     let date_enter = Date.now()
+    const ipAddress = IP.address()
     await sql`
         insert into history (
             ip, date_enter, city , phone
         ) values (
-            ${req.socket.remoteAddress}, ${date_enter},'Минск',${req.query.phone}
+            ${ipAddress}, ${date_enter},'Минск',${req.query.phone}
         )
     `
    
