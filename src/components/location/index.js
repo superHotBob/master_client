53c2e4198294732bd83dfabbdb9f32c453ad7f74
superHotBob <19ham09@gmail.com>
2023-08-07 16:30:18 +0300

    add_new

--- src/components/location/index.js
+++ src/components/location/index.js
@@ -1,43 +1,31 @@
 import styles from './location.module.css'
 import { useEffect, useRef } from 'react'
-import React from 'react';
-import Script from 'next/script'
+import React from 'react'
 import { useDispatch } from 'react-redux'
 import { setlocation } from '@/reduser'
 import Image from 'next/image'
-// import { YMaps, Map, Placemark, withYMaps, useYMaps } from '@pbe/react-yandex-maps'
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps'
 import icon_close from '../../../public/close.svg'
-import { load } from '@2gis/mapgl';
+// import { load } from '@2gis/mapgl';
 
-//   const SuggestComponent = () => {
-//     return withYMaps(MapSuggestComponent, true, [
-//       "SuggestView",
-//       "geocode",
-//       "coordSystem.geo"
-//     ]);
-//   };
 
-export const Map = () => {
-    useEffect(() => {
-        let map;
-        load().then((mapglAPI) => {
-            map = new mapglAPI.Map('container', {
-                center: [55.31878, 25.23584],
-                zoom: 13,
-                key: 'c080bb6a-8134-4993-93a1-5b4d8c36a59b',
-            });
-        });
 
-        // Destroy the map on unmount
-        return () => map && map.destroy();
-    }, []);
-}
-const MapWrapper = React.memo(
-    () => {
-        return <div id="container" style={{ width: '100%', height: '400px' }}></div>;
-    },
-    () => true,
-);
+// export const Map = () => {
+//     useEffect(() => {
+//         let map;
+//         load().then((mapglAPI) => {
+//             map = new mapglAPI.Map('container', {
+//                 center: [55.31878, 25.23584],
+//                 zoom: 13,
+//                 key: 'c080bb6a-8134-4993-93a1-5b4d8c36a59b',
+//             });
+//         });
+
+//         // Destroy the map on unmount
+//         return () => map && map.destroy();
+//     }, []);
+// }
+
 const API_KEY = "89caab37-749d-4e30-8fdf-e8045542f060"
 export default function Location({ loc_master, close, nikname }) {
     const dispatch = useDispatch()
@@ -73,51 +61,48 @@ export default function Location({ loc_master, close, nikname }) {
 
     return (
         <div className={styles.map}>
-            {/* <Script src={`https://api-maps.yandex.ru/3.0/?apikey=${API_KEY}&lang=ru_RU`} /> */}
-            {/* <div style={{ width: '100%', height: '400px' }}>
-                <div id="container"/>
-            </div> */}
-           
+
+
             <div className={styles.my_map} >
-                {/* <Image src={icon_close} onClick={()=>close(false)} alt="close" width={20} height={20}/>
-                        <YMaps >                       
-                            <Map id="mymap"
-                                options={{ set: defaultState }}
-                                state={{
-                                    center: loc_master ,
-                                    zoom:  12 ,
-                                    controls: [],
-                                    behaviors: ["default", "scrollZoom","multiTouch","drag","onclick"]
-                                }} 
-                                width='100%' 
-                                height= "75vh"                                      
-                               
-                            >
-                               
-                                <Placemark geometry={loc_master} 
-                                 modules= {
-                                    ['geoObject.addon.balloon', 'geoObject.addon.hint']
-                                }
-                                   
-                                    options={{
-                                        draggable: true,
-                                        iconLayout: 'default#image',
-                                        iconImageHref: '/master.svg',
-                                        iconImageSize: [40, 40],
-                                        preset: "islands#yellowDotIcon",
-                                    }}
-                                    onLoad={()=>ViewGrayScale()}
-                                    onDragStart={(e) =>
-                                        console.log(e.get("target").geometry.getCoordinates())
-                                      }
-                                    onDragEnd={(e) =>{
-                                        console.log(e.get("target").geometry.getCoordinates())
-                                        dispatch(setlocation(e.get("target").geometry.getCoordinates()))
-                                      }}  
-                                    onClick={(e) => UpdateLocation(e.get("target").geometry.getCoordinates())}
-                                />
-                            </Map>
-                        </YMaps> */}
+                <Image src={icon_close} onClick={() => close(false)} alt="close" width={20} height={20} />
+                <YMaps >
+                    <Map id="mymap"
+                        options={{ set: defaultState }}
+                        state={{
+                            center: loc_master,
+                            zoom: 12,
+                            controls: [],
+                            behaviors: ["default", "scrollZoom", "multiTouch", "drag", "onclick"]
+                        }}
+                        width='100%'
+                        height="75vh"
+
+                    >
+
+                        <Placemark geometry={loc_master}
+                            modules={
+                                ['geoObject.addon.balloon', 'geoObject.addon.hint']
+                            }
+
+                            options={{
+                                draggable: true,
+                                iconLayout: 'default#image',
+                                iconImageHref: '/master.svg',
+                                iconImageSize: [40, 40],
+                                preset: "islands#yellowDotIcon",
+                            }}
+                            onLoad={() => ViewGrayScale()}
+                            onDragStart={(e) =>
+                                console.log(e.get("target").geometry.getCoordinates())
+                            }
+                            onDragEnd={(e) => {
+                                console.log(e.get("target").geometry.getCoordinates())
+                                dispatch(setlocation(e.get("target").geometry.getCoordinates()))
+                            }}
+                            onClick={(e) => UpdateLocation(e.get("target").geometry.getCoordinates())}
+                        />
+                    </Map>
+                </YMaps>
             </div>
 
         </div>
