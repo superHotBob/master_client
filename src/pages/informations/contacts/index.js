31c81f932584d48c58db50678e313c43e8ec6aa6
superHotBob <19ham09@gmail.com>
2024-02-05 13:13:53 +0300

    next_commit

--- src/pages/informations/contacts/index.js
+++ src/pages/informations/contacts/index.js
@@ -7,18 +7,23 @@ export default function contacts() {
 
     const router = useRouter()
 
+    const back = () => router.back()
+
+
     return (
         <main className={styles.main}>
             <header className={styles.header}>
-                <Image src={arrow} alt="back" onClick={() => router.back()} />
+                <Image src={arrow} alt="back" onClick={back} />
                 <h4>Контакты</h4>
                 <h4 onClick={() => router.push("/")}>Закрыть</h4>
             </header>
             <section className={styles.links}>
                 <h3>Контакты</h3>
-                <p><dd>Tелефон:</dd> <dt>+375 00 000 00 00</dt></p>
-                <p><dd>Телефон:</dd> <dt>+375 11 000 00 00</dt></p>
-                <p><dd>Почта:</dd> <dt>support@masters.place</dt></p>               
+                <dd>Tелефон:</dd> <dt>+375 00 000 00 00</dt>
+                <br/>
+                <dd>Телефон:</dd> <dt>+375 11 000 00 00</dt>
+                <br/>
+                <dd>Почта:</dd> <dt>support@masters.place</dt>            
             </section>
         </main>
     )
